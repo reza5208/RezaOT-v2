06712d30ba1c520ea020e39bedc8eff6c1dda29c
reza5208 <northman.reza@gmail.com>
2026-05-17 16:58:47 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,47 +1,42 @@
-// sw.js - RezaOT Service Worker v6
-const CACHE_NAME = "rezaot-v6";   // Naikkan version bila update
+// sw.js - RezaOT v7
+const CACHE_NAME = "rezaot-v7";
 
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./styles.css",
-  "./main.js",
-  "./utils.js",
-  "./constants.js",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png",
-  "./apple-icon-180.png"
+    "./",
+    "./index.html",
+    "./styles.css",
+    "./main.js",
+    "./utils.js",
+    "./constants.js",
+    "./manifest.json",
+    "./assets/icons/icon-192.png",
+    "./assets/icons/icon-512.png",
+    "./assets/icons/apple-icon-180.png"
 ];
 
 self.addEventListener("install", (event) => {
-  console.log("✅ RezaOT Service Worker installing... v6");
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+    console.log("✅ Installing RezaOT Service Worker v7...");
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    );
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            console.log("Deleting old cache:", cache);
-            return caches.delete(cache);
-          }
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) return caches.delete(cache);
+                })
+            );
         })
-      );
-    })
-  );
+    );
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
 });
