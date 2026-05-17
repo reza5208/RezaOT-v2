d6f7084f3e7b9d7449f822494fae6310cc496d45
reza5208 <northman.reza@gmail.com>
2026-05-17 16:23:44 +0800

    Update service worker to version 5 with new cache

--- sw.js
+++ sw.js
@@ -1,41 +1,41 @@
-const CACHE_NAME = "pwe-overtime-v4";
+const CACHE_NAME = "rezaot-v5";   // Naikkan version
 
 const urlsToCache = [
-    "/index.html",
-    "/styles.css",
-    "/constants.js",
-    "/utils.js",
-    "/main.js",
-    "/manifest.json",
-    "/assets/icons/favicon.ico",
-    "/assets/icons/apple-icon-57x57.png",
-    "/assets/icons/android-icon-192x192.png",
-    "/assets/icons/android-icon-512x512.png"
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./constants.js",
+  "./utils.js",
+  "./main.js",
+  "./manifest.json",
+  "./assets/icons/android-icon-192x192.png",
+  "./assets/icons/android-icon-512x512.png",
+  "./assets/icons/apple-icon-180x180.png"
 ];
 
 self.addEventListener("install", event => {
-    console.log("Installing service worker v4...");
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-    );
+  console.log("✅ Installing RezaOT Service Worker v5...");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
 
 self.addEventListener("activate", event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cache => {
-                    if (cache !== CACHE_NAME) return caches.delete(cache);
-                })
-            );
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) return caches.delete(cache);
         })
-    );
+      );
+    })
+  );
 });
 
 self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
