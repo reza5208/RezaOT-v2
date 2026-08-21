e291ad1a90303d74aa487681bc8441b4117cf7c5
reza5208 <northman.reza@gmail.com>
2026-08-21 16:45:58 +0800

    fix(print): date/day nowrap + wider columns, clean subtitle overflow; SW v13

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// sw.js - RezaOT v12
-const CACHE_NAME = "rezaot-v12";
+// sw.js - RezaOT v13
+const CACHE_NAME = "rezaot-v13";
 
 const urlsToCache = [
   "./",
@@ -12,7 +12,7 @@ const urlsToCache = [
 ];
 
 self.addEventListener("install", (event) => {
-  console.log("Installing RezaOT Service Worker v12...");
+  console.log("Installing RezaOT Service Worker v13...");
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => cache.addAll(urlsToCache))
