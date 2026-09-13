fefb17aa1ef4bb6963bc7d532f33e876391fda5e
reza5208 <northman.reza@gmail.com>
2026-09-13 22:25:36 +0800

    v51: cache print-dense + print-fix in SW

--- sw.js
+++ sw.js
@@ -2,11 +2,11 @@
 const CACHE_NAME = "rezaot-v51";
 const urlsToCache = [
   "./", "./index.html",
-  "./styles.css", "./styles-print.css", "./styles-print-v39.css",
+  "./styles.css", "./styles-print.css", "./styles-print-v39.css", "./print-dense-v51.css",
   "./styles-v18-extra.css", "./styles-salary.css", "./styles-v27.css",
   "./styles-header-fix.css", "./styles-ux-v38.css", "./styles-app-v42.css",
   "./main.js", "./main-app-1.js", "./main-app-2.js", "./app-p1.js", "./app-p1-extra.js",
-  "./app-v42-overlay.js", "./side-job-patch.js", "./sync-fix-v48.js", "./month-fix.js",
+  "./app-v42-overlay.js", "./side-job-patch.js", "./sync-fix-v48.js", "./month-fix.js", "./print-fix-v51.js",
   "./i18n.js", "./modal.js", "./salary-estimator.js", "./utils.js", "./constants.js", "./holiday-picker.js",
   "./manifest.json",
   "./assets/icons/favicon-32x32.png", "./assets/icons/android-icon-192x192.png"
